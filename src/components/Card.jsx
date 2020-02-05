90f6fe63cf32c653abee51d7612cf9f4a241880c
MrDean <Paul---@users.noreply.github.com>
2020-02-05 13:03:12 -0700

    change project name

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img src="#" alt="Photo" />
+      <div>
+        <h2>Jimmy eat World</h2>
+        <p>fake@email.com</p>
+      </div>
+    </div>
+  )
+}
