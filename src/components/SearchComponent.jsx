275e68e3842af4b414de67c9af5309fd8ae48058
MrDean <Paul---@users.noreply.github.com>
2020-02-06 11:56:28 -0700

    add margin to bottom of search bar

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 const SearchComponent = ({ searchField, changeSearchField }) => {
-
   const updateSearchTerm = e => {
     changeSearchField(e.target.value);
   };
-  
+
   const placeholder = `Search Contacts Here`;
-  
+
   return (
-    <div>
-      <input 
+    <div style={{ paddingBottom: "8px" }}>
+      <input
         className="tc"
         type="search"
         value={searchField}
