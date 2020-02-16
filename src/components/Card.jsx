c78812b24ed5761e1b24bfe747cb3bb31700a08f
MrDean <Paul---@users.noreply.github.com>
2020-02-15 18:50:59 -0700

    change minor layout details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,8 @@ const Card = ({ nameFirst, nameLast, pictureUrl, email }) => {
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       {email ? <img src={pictureUrl} alt="Contact" /> : null}
       <div>
-        <h2>{nameFirst}</h2>
-        <h2>{nameLast}</h2>
-        <p>{email}</p>
+        <h2>{nameFirst} {nameLast}</h2>
+        <h4>{email}</h4>
       </div>
     </div>
   );
--- src/components/Scoller.jsx
+++ src/components/Scoller.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Scroller = ({ children }) => {
-  return <div style={{ overflowY: "scroll", border: '1px solid black', height:'600px' }}>{children}</div>;
+  return <div style={{ overflowY: "scroll", border: '1px solid black', height:'700px' }}>{children}</div>;
 };
 
 export default Scroller;
